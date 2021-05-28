4a5a8511ad9b4e7f3da784bf23ea7168dc1fd858
saracrz <sara.crz10@gmail.com>
2021-05-28 15:43:16 +0100

    Add Input, utton and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input/Input'
 
 
 function App() {
@@ -7,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Input/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,6 @@
+import './Button.css'
+
+const Button = (): JSX.Element => <button className='button'>Add</button>
+
+
+export default Button;
